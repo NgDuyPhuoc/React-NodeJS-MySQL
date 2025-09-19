6afaf8215662e7a05b640c8e331333f3333793ce
NgDuyPhuoc <ngduyphuoc25082006@gmail.com>
2025-09-19 22:46:12 +0700

    update

---
+++ Buoi-6/quanly_sinhvien.sql
@@ -0,0 +1,8 @@
+SELECT * FROM quanly_sinhvien.sinhvien;
+
+INSERT INTO quanly_sinhvien.sinhvien (ten, tuoi, lop, email) VALUES
+('Nguyễn Duy Phước', 19, 'CNTT', 'ngduyphuoc25082006@gmail.com'),
+('Dương Thúy Kiều', 19, 'NNA', 'duongthuykieu10032006@gmail.com'),
+('Cao Thiên Quí', 19, 'CNTT', 'caothienqui1801@gmail.com'),
+('Nguyễn Minh Toàn', 19, 'CNTT', 'ngminhtoan@gmail.com'),
+('Nguyễn Thị Huyền Trân', 19, 'CNTT', 'ngthihuyentran@gmail.com');
